a5d1ad40d182b9b8d31d2dcd7f8231c07d0101b4
Mousa Ahmed <moussa.abdelghany@gmail.com>
2024-10-25 15:06:32 +0300

    feat: setup toaster & spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgxSpinnerModule } from "ngx-spinner";
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
     MatSlideToggleModule,
+    NgxSpinnerModule
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [
+  ],
 })
 export class AppComponent {
   title = 'tasks-user';
+  constructor() {
+   }
 }
